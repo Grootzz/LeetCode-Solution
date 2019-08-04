1e86be1b6be5ba0b213046bc272c4f39e49642a1
Grootzz <noodle.ax@gmail.com>
2019-08-04 13:49:07 +0800

    95. 不同的二叉搜索树 II

--- src/main/java/leetcode/solution/dp/q95/Solution.java
+++ src/main/java/leetcode/solution/dp/q95/Solution.java
@@ -2,6 +2,7 @@ package leetcode.solution.dp.q95;
 
 import util.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,8 +13,38 @@ import java.util.List;
 class Solution {
 
     public List<TreeNode> generateTrees(int n) {
-        return null;
+        if (n == 0)
+            return new ArrayList<TreeNode>();
+
+        List<TreeNode> allTrees = generateTrees(1, n);
+        return allTrees;
     }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new ArrayList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftTree = generateTrees(start, i - 1);
+            List<TreeNode> rightTree = generateTrees(i + 1, end);
+
+            // 组合左右子树
+            for (TreeNode left : leftTree) {
+                for (TreeNode right : rightTree) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    allTrees.add(root);
+                }
+            }
+        }
+
+        return allTrees;
+    }
+
 }
 
 
