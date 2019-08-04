cb33108dff46bf40971a152a156d35b521eeb2fc
Grootzz <noodle.ax@gmail.com>
2019-08-04 15:49:34 +0800

    97. 交错字符串

--- src/main/java/leetcode/solution/dp/q97/Solution.java
+++ src/main/java/leetcode/solution/dp/q97/Solution.java
@@ -1,13 +1,50 @@
 package leetcode.solution.dp.q97;
 
+import org.junit.Test;
+
 /**
  * 97. 交错字符串
  * <p>
  * https://leetcode-cn.com/problems/interleaving-string/
+ * https://leetcode-cn.com/problems/interleaving-string/solution/jiao-cuo-zi-fu-chuan-by-leetcode/
  *
  * @author noodle
  * @date 2019/8/4 15:01
  */
 public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s3.length() != (s1.length() + s2.length()))
+            return false;
 
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 0; i < s1.length() + 1; i++) {
+            for (int j = 0; j < s2.length() + 1; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] && (s2.charAt(j - 1) == s3.charAt(j - 1));
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] && (s1.charAt(i - 1) == s3.charAt(i - 1));
+                } else {
+                    dp[i][j] = (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1))
+                            || (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+
+
+    @Test
+    public void doTest() {
+        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
+        System.out.println(isInterleave(s1, s2, s3));
+    }
+
+    @Test
+    public void doTest01() {
+        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc";
+        System.out.println(isInterleave(s1, s2, s3));
+    }
 }
