260b0fd5e8248d60929dbd18b34b488fae6a44bf
Grootzz <noodle.ax@gmail.com>
2019-08-18 10:51:03 +0800

    22. 括号生成

--- src/main/java/leetcode/solution/backtrace/q17/Solution.java
+++ src/main/java/leetcode/solution/backtrace/q17/Solution.java
@@ -3,53 +3,40 @@ package leetcode.solution.backtrace.q17;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
- * 17. 电话号码的字母组合
- * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ * 22. 括号生成
+ * https://leetcode-cn.com/problems/generate-parentheses/
+ * 回溯+剪枝
  *
  * @author noodle
  * @date 2019/8/18 09:49
  */
 public class Solution {
-    public List<String> letterCombinations(String digits) {
-
-        Map<Character, char[]> map = new HashMap<>();
-        map.put('2', new char[]{'a', 'b', 'c'});
-        map.put('3', new char[]{'d', 'e', 'f'});
-        map.put('4', new char[]{'g', 'h', 'i'});
-        map.put('5', new char[]{'j', 'k', 'l'});
-        map.put('6', new char[]{'m', 'n', 'o'});
-        map.put('7', new char[]{'p', 'q', 'r', 's'});
-        map.put('8', new char[]{'t', 'u', 'v'});
-        map.put('9', new char[]{'w', 'x', 'y', 'z'});
-
+    public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
-        backtrace(map, ans, "", digits);
-
+        backtrace(ans, "", 0, 0, n);
         return ans;
     }
 
-    private void backtrace(Map<Character, char[]> map, List<String> ans, String subStr, String subDigits) {
-        if (subDigits.length() == 0) {
-            ans.add(subStr);
-        } else {
-            char c = subDigits.charAt(0);
-            char[] letters = map.get(c);
-
-            for (int i = 0; i < letters.length; i++) {
-                backtrace(map, ans, subStr + letters[i], subDigits.substring(1));
-            }
+    private void backtrace(List<String> ans, String sub, int open, int close, int n) {
+        if (sub.length() == 2 * n) {
+            ans.add(sub);
+            return;
         }
+
+        if (open < n)
+            backtrace(ans, sub + "(", open + 1, close, n);
+
+        if (close < open)
+            backtrace(ans, sub + ")", open, close + 1, n);
+
     }
 
     @Test
     public void doTest() {
-        String digs = "23";
-        List<String> ans = letterCombinations(digs);
+        List<String> ans = generateParenthesis(3);
         System.out.println(ans);
     }
 }
---
+++ src/main/java/leetcode/solution/backtrace/q22/Solution.java
@@ -0,0 +1,55 @@
+package leetcode.solution.backtrace.q22;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 17. 电话号码的字母组合
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ *
+ * @author noodle
+ * @date 2019/8/18 09:49
+ */
+public class Solution {
+    public List<String> letterCombinations(String digits) {
+
+        Map<Character, char[]> map = new HashMap<>();
+        map.put('2', new char[]{'a', 'b', 'c'});
+        map.put('3', new char[]{'d', 'e', 'f'});
+        map.put('4', new char[]{'g', 'h', 'i'});
+        map.put('5', new char[]{'j', 'k', 'l'});
+        map.put('6', new char[]{'m', 'n', 'o'});
+        map.put('7', new char[]{'p', 'q', 'r', 's'});
+        map.put('8', new char[]{'t', 'u', 'v'});
+        map.put('9', new char[]{'w', 'x', 'y', 'z'});
+
+        List<String> ans = new ArrayList<>();
+        backtrace(map, ans, "", digits);
+
+        return ans;
+    }
+
+    private void backtrace(Map<Character, char[]> map, List<String> ans, String subStr, String subDigits) {
+        if (subDigits.length() == 0) {
+            ans.add(subStr);
+        } else {
+            char c = subDigits.charAt(0);
+            char[] letters = map.get(c);
+
+            for (int i = 0; i < letters.length; i++) {
+                backtrace(map, ans, subStr + letters[i], subDigits.substring(1));
+            }
+        }
+    }
+
+    @Test
+    public void doTest() {
+        String digs = "23";
+        List<String> ans = letterCombinations(digs);
+        System.out.println(ans);
+    }
+}
