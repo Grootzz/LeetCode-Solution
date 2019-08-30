18ee4324e902b1bb65afa8cf6d3e28b16d6f6d1c
Grootzz <noodle.ax@gmail.com>
2019-08-30 13:18:06 +0800

    662. 二叉树最大宽度

--- src/main/java/leetcode/solution/tree/q662/Solution.java
+++ src/main/java/leetcode/solution/tree/q662/Solution.java
@@ -2,6 +2,8 @@ package leetcode.solution.tree.q662;
 
 import util.TreeNode;
 
+import java.util.*;
+
 /**
  * 662. 二叉树最大宽度
  * https://leetcode-cn.com/problems/maximum-width-of-binary-tree/
@@ -10,9 +12,56 @@ import util.TreeNode;
  * @date 2019/8/30 09:39
  */
 public class Solution {
-//    public int widthOfBinaryTree(TreeNode root) {
-//
-//    }
 
+    /**
+     * 迭代
+     */
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null)
+            return 0;
 
+        int width = 0;
+
+        Queue<Entry> queue = new LinkedList<>();
+
+        queue.add(new Entry(root, 1));
+        while (!queue.isEmpty()) {
+
+            int start = queue.peek().getId();
+            int end = start;
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.peek().getNode();
+                end = queue.poll().getId();
+
+                if (node.left != null)
+                    queue.offer(new Entry(node.left, end * 2));
+                if (node.right != null)
+                    queue.offer(new Entry(node.right, end * 2 + 1));
+            }
+
+            width = Math.max(width, end - start + 1);
+        }
+
+        return width;
+    }
+
+    private class Entry {
+        private TreeNode node;
+        private int id;
+
+        public Entry(TreeNode node, int id) {
+            this.node = node;
+            this.id = id;
+        }
+
+        public TreeNode getNode() {
+            return node;
+        }
+
+        public int getId() {
+            return id;
+        }
+    }
 }
