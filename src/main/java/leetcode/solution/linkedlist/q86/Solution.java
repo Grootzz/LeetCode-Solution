0976eaf2df4b4e95b0318d75557ebab3947b95de
Grootzz <noodle.ax@gmail.com>
2019-09-01 22:48:37 +0800

    86. 分隔链表

---
+++ src/main/java/leetcode/solution/linkedlist/q86/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.solution.linkedlist.q86;
+
+import util.ListNode;
+
+/**
+ * 86. 分隔链表
+ *
+ * @author noodle
+ * @date 2019/9/1 22:38
+ */
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode smaller = new ListNode(-1);
+        ListNode smallerTmp = smaller;
+        ListNode larger = new ListNode(-1);
+        ListNode largerTmp = larger;
+
+        while (head != null) {
+            if (head.val < x) {
+                smaller.next = head;
+                smaller = smaller.next;
+            } else {
+                larger.next = head;
+                larger = larger.next;
+            }
+            head = head.next;
+        }
+
+        larger.next = null;
+        smaller.next = largerTmp.next;
+
+        return smallerTmp.next;
+    }
+}
